de24a858d77504538847a6fa8c4551be154c89c2
Gianni Zito <giannizito@msn.com>
2018-07-29 19:04:19 +0200

    Initial Commit

---
+++ PieShop/Controllers/PieController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+using PieShop.ViewModels;
+
+namespace PieShop.Controllers
+{
+    public class PieController : Controller
+    {
+
+        private IPieRepository _pieRepository;
+        private ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {
+
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.Pies;
+            piesListViewModel.CurrentCategory = "Cheese Cake";
+
+            return View(piesListViewModel);
+        }
+    }
+}
---
+++ PieShop/README.md
@@ -0,0 +1,2 @@
+# PieShop
+MCCore
